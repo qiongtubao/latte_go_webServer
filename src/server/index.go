3be90fadc8a51ce86ee3a2d193e4e9f717bd1089
qiongtubao <810776886@qq.com>
2019-01-03 01:46:10 +0800

    update

--- src/server/index.go
+++ src/server/index.go
@@ -9,15 +9,19 @@ type Server struct {
 	Methods map[string]func(http.ResponseWriter, *http.Request)
 	mux *http.ServeMux
 }
+func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	method := s.Methods[r.URL.Path]
+	if method != nil {
+		method(w, r)
+	} else {
+		http.NotFound(w, r)
+	}
+}
 func (s Server) AddHandle(name string, method func(http.ResponseWriter, *http.Request)) {
     s.Methods[name] = method                           
 }
 func (s Server) Start() {
-	for name := range s.Methods {
-		// fmt.Println(country, "首都是", s.mux [ name ])
-		s.mux.HandleFunc(name, s.Methods[name])
-	}
-	http.ListenAndServe(":" + lib.IntToStr(s.Port), s.mux)
+	http.ListenAndServe(":" + lib.IntToStr(s.Port), s)
 }
 func CreateServer() Server {
 	m := map[string]func(http.ResponseWriter, *http.Request){}
